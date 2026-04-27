4c2da30370a2212417b1d36d47639715680e6eb1
ValeriyAlexeyev <innuaki@yandex.ru>
2026-04-27 15:20:20 +0500

    Implement isMorse function for input validation
    Add isMorse function to validate Morse code input.

--- internal/service/service.go
+++ internal/service/service.go
@@ -16,10 +16,24 @@ func Convert(input string) (string, error) {
 		return "", ErrEmptyInput
 	}
 
-	// если есть точка или тире → считаем что это Morse
-	if strings.Contains(input, ".") || strings.Contains(input, "-") {
+	if isMorse(input) {
 		return morse.ToText(input), nil
 	}
 
 	return morse.ToMorse(input), nil
+}
+
+func isMorse(s string) bool {
+	for _, r := range s {
+		if r != '.' &&
+			r != '-' &&
+			r != ' ' &&
+			r != '/' &&
+			r != '\n' &&
+			r != '\r' &&
+			r != '\t' {
+			return false
+		}
+	}
+	return true
 }
